81f909ae627227eff1652018603ec21cc1874d04
Uzma Khan <meow414@users.noreply.github.com>
2016-08-19 19:39:41 +0530

    document ready added

--- random.js
+++ random.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 $('.Yes').click(function() {
 
   // start get - URI (REPLACE KEY WITH YOUR OWN, callback function)
@@ -9,26 +10,7 @@ $('.Yes').click(function() {
     $('.results').html("<h2>" + entry.gsx$quote.$t + "</h2>");
     $("#w").attr("href", 'https://twitter.com/intent/tweet?text=' + entry.gsx$quote.$t);
   });
-  // $("#w").append($(".results"));
+ 
 
 });
-/*$("#w").click(function(){
-$(".results").clone.append("#w");
 });
-*/
-/*$(".twitter-share-button").click(function(){
-  // start get - URI (REPLACE KEY WITH YOUR OWN, callback function)
-
-  // see http://coderwall.com/p/duapqq for tutorial on using Google Spreadsheet as JSON
-  $.getJSON("https://spreadsheets.google.com/feeds/list/1tBLZMDQovKmRGeMKyVqFSOCSLEZyiML42hw__5_eZZE/od6/public/values?alt=json", function(data) {
-  //start each
-    var entry = data.feed.entry[Math.floor(Math.random()*data.feed.entry.length)];
-
-$("#w").attr("href", 'https://twitter.com/intent/tweet?text=' + entry.gsx$quote.$t );// And only this part was needed but in above code,an yeah $(this).attr(); will not work.Used #w therefore!
-
-  });
-
-  });
-This part doesn't work but it taught me why the above code(uncommnted part) will work.  :D
-    It was producing diefferent quote.
-*/
